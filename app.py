4791385f11b91cdabfa79591280f0a9fc784f3b7
Viktor Likin <viktor.likin@gmail.com>
2014-07-31 09:22:14 +0300

    Video 1.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import flask.views
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class View(flask.views.MethodView):
+    def get(self):
+        return 'Hello World!'
+
+app.add_url_rule('/hi', 'main', view_func=View.as_view('main'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
