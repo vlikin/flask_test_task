620a3bc54b7e08ad68e003a2f0291cd3ed528308
Viktor Likin <viktor.likin@gmail.com>
2014-07-31 13:01:58 +0300

    Video 3: Login.

--- app.py
+++ app.py
@@ -1,20 +1,61 @@
 import flask, flask.views
 from flask import Flask
 import os
+import functools
 
 app = Flask(__name__)
 app.secret_key = 'key'
 
-class View(flask.views.MethodView):
+def login_required(method):
+    @functools.wraps(method)
+    def wrapper(*args, **kargs):
+        if 'username' in flask.session:
+            return method(*args, **kargs)
+        else:
+            flask.flash('A login is required to see a page.')
+            return flask.redirect(flask.url_for('index'))
+
+    return wrapper
+
+class Index(flask.views.MethodView):
     def get(self):
         return flask.render_template('index.html')
 
+    def post(self):
+        if 'logout' in flask.request.form:
+            flask.session.pop('username', None)
+            return self.get()
+        users = {
+            'viktor': 'viktor',
+            'admin': 'admin'
+        }
+        required = ['username', 'password']
+        for field in required:
+            if field not in flask.request.form:
+                flask.flash('Error: {0} is required'.format(field))
+                return flask.redirect(flask.url_for('index'))
+        username = flask.request.form['username']
+        password = flask.request.form['password']
+        if username in users and users[username] == password:
+            flask.session['username'] = username
+        else:
+            flask.flash('Username does not exists or incorrect password.');
+        return flask.redirect(flask.url_for('index'))
+
+class Remote(flask.views.MethodView):
+
+    @login_required
+    def get(self):
+        return flask.render_template('remote.html')
+
+    @login_required
     def post(self):
         result = eval(flask.request.form['expression'])
         flask.flash(result)
         return self.get()
 
-app.add_url_rule('/', 'main', view_func=View.as_view('main'), methods=['GET', 'POST'])
+app.add_url_rule('/', view_func=Index.as_view('index'), methods=['GET', 'POST'])
+app.add_url_rule('/remote', view_func=Remote.as_view('remote'), methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
