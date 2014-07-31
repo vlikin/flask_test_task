1104678a8448a9f9ab965e229524f71bde5fe54c
Viktor Likin <viktor.likin@gmail.com>
2014-07-31 10:13:46 +0300

    Video 2: Dangereous stuff.

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-import flask.views
+import flask, flask.views
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app.secret_key = 'key'
 
 class View(flask.views.MethodView):
     def get(self):
-        return 'Hello World!'
+        return flask.render_template('index.html')
 
-app.add_url_rule('/hi', 'main', view_func=View.as_view('main'))
+    def post(self):
+        result = eval(flask.request.form['expression'])
+        flask.flash(result)
+        return self.get()
+
+app.add_url_rule('/', 'main', view_func=View.as_view('main'), methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
